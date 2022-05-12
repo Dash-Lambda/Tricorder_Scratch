db96dd7bba1dcc42ee5b9956583e8735b487d098
Dash-Lambda <dashlambda@gmail.com>
2022-05-12 15:27:22 -0400

    line endings...

--- setup_stuff.sh
+++ setup_stuff.sh
@@ -1,8 +1,7 @@
-sudo cp setup_files/apt-radxa-com.list /etc/apt/sources.list.d/apt-radxa-com.list
+sudo cp "setup_files/apt-radxa-com.list" "/etc/apt/sources.list.d/apt-radxa-com.list"
 
 wget -O - apt.radxa.com/buster-stable/public.key | sudo apt-key add -
 wget -O - apt.radxa.com/buster-testing/public.key | sudo apt-key add -
 
 sudo apt update
 sudo apt install libmraa build-essential
-
