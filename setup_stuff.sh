2086d8777ee4b77c69ffb414e479718d45a99a04
Dash-Lambda <dashlambda@gmail.com>
2022-05-12 15:19:20 -0400

    maybe copy will work...

--- setup_stuff.sh
+++ setup_stuff.sh
@@ -1,10 +1,7 @@
-export DISTRO=buster-stable
-echo "deb http://apt.radxa.com/$DISTRO/ ${DISTRO%-*} main" | sudo tee -a /etc/apt/sources.list.d/apt-radxa-com.list
-wget -O - apt.radxa.com/$DISTRO/public.key | sudo apt-key add -
+sudo cp setup_files/apt-radxa-com.list /etc/apt/sources.list.d/apt-radxa-com.list
 
-export DISTRO=buster-testing
-echo "deb http://apt.radxa.com/$DISTRO/ ${DISTRO%-*} main" | sudo tee -a /etc/apt/sources.list.d/apt-radxa-com.list
-wget -O - apt.radxa.com/$DISTRO/public.key | sudo apt-key add -
+wget -O - apt.radxa.com/buster-stable/public.key | sudo apt-key add -
+wget -O - apt.radxa.com/buster-testing/public.key | sudo apt-key add -
 
 sudo apt update
 sudo apt install libmraa build-essential
